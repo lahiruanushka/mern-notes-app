7a8767746146e1d12343dd94cf4bb70cecbf7e56
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-09-02 17:43:35 +0530

    feat: implement CRUD routes for notes

---
+++ backend/src/models/note.ts
@@ -0,0 +1,10 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+const notesSchema = new Schema({
+    title: { type: String, required: true},
+    text: { type: String},
+}, { timestamps: true});
+
+type Note = InferSchemaType<typeof notesSchema>;
+
+export default model<Note>("Note", notesSchema);
---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+import mongoose from "mongoose";
+import env from "./utill/validateEnv";
+
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Monogdb connected...");
+    app.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+    });
+  })
+  .catch(console.error);
