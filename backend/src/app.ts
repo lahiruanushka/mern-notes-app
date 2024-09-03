44a83dc021b047059743c254da9648a654dc1d10
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-09-03 12:33:02 +0530

    chore: Enable CORS using the cors npm package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,20 +2,20 @@ import "dotenv/config";
 import express, { Request, Response, NextFunction } from "express";
 import notesRoutes from "./routes/noteRoutes";
 import morgan from "morgan";
-import createHttpError, { isHttpError} from "http-errors";
+import createHttpError, { isHttpError } from "http-errors";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
-  next(createHttpError(404,"Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
-
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -24,9 +24,9 @@ app.use(
     let errorMessage = "An unknown error occurred";
     let statusCode = 500;
 
-    if(isHttpError(error)){
-        statusCode = error.status;
-        errorMessage = error.message;
+    if (isHttpError(error)) {
+      statusCode = error.status;
+      errorMessage = error.message;
     }
     res.status(statusCode).json({ error: errorMessage });
   }
